2423db85368bfa6cb7d94b02db4b1e9cec333d73
DarkyyyyyP <2014069533@ust-ics.mygbiz.com>
2014-11-07 21:58:50 +0800

    Create PTRJON.C

---
+++ PTRJON.C
@@ -0,0 +1,49 @@
+1st-Year-TURBOC
+===============
+
+#include <stdio.h>
+#include <conio.h>
+#define MAX 5
+
+void swap(int *a, int *b)
+{
+	int *temp;
+	*temp = *a;
+	*a = *b;
+	*b = *temp;
+}
+
+void main()
+{
+	int arr[100],x,y,temp;
+    clrscr();
+	printf("\nEnter the %d elements: ", MAX);
+	for(x=0;x < MAX;x++)
+	{
+		scanf("%d",&arr[x]);
+	}
+	for(x=0; x < MAX; x++)
+	{
+		for (y = x; y < MAX; y++)
+		{
+			if (arr[x] > arr[y]) //ASCENDING, REV IF DESCEND
+			{
+				swap(&arr[x],&arr[y]);
+			}
+		}
+	}
+	printf("\nAscending: ");
+	for(x=0;x<MAX;x++)
+	{
+		printf("\t%d",arr[x]);
+	}
+
+	printf("\nDescending: ");
+
+	for (x=MAX-1; x>-1; x--) //Reverse Looping *With minus 1 kasi masasama yung 0 val
+	{
+		printf("\t%d", arr[x]);
+	}
+	getch();
+}
+
